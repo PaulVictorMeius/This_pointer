b8ce0b3faeab9f88bcc97d2d172df6126de95daf
Paul Victor Meius <paulmeius@yahoo.com>
2021-09-09 15:21:24 +0300

    Added multiplication method

--- A.h
+++ A.h
@@ -5,10 +5,11 @@ class A
 public:
 	explicit A(int m) :m{ m } {}
 	void set_m(int m) { this->m = m; }
-	auto get_m()const { return m; }
+	int get_m()const { return m; }
 	void print_m()const { std::cout << "m=" << m << '\n'; }
 	A& add(int n) { m += n; return *this; }
 	A& sub(int n) { m -= n; return *this; }
+	A& mult(int n) { m *= n; return *this; }
 private:
 	int m;
 };
