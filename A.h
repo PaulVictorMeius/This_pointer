4ae4ccadcbbc3ad33e8c541e4d3d853e1cfad068
Paul Victor Meius <paulmeius@yahoo.com>
2021-09-09 15:21:24 +0300

    Changed the return type

--- A.h
+++ A.h
@@ -5,7 +5,7 @@ class A
 public:
 	explicit A(int m) :m{ m } {}
 	void set_m(int m) { this->m = m; }
-	auto get_m()const { return m; }
+	int get_m()const { return m; }
 	void print_m()const { std::cout << "m=" << m << '\n'; }
 	A& add(int n) { m += n; return *this; }
 	A& sub(int n) { m -= n; return *this; }
